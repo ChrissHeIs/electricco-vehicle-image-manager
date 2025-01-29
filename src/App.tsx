2220f8c1b426641859abe0b8c7428b784c46f6cf
Pavlo Shestak <shestak.pavel@gmail.com>
2025-01-29 20:49:48 +0200

    Exporting json instead of images

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import CsvUploader from "./components/CsvUploader"; // Handles CSV parsing
 import VehicleImageFetchingList from "./components/VehicleImageFetchingList"; // Handles image fetching
 import { Vehicle } from "./model/Vehicle"; // Assuming Vehicle type is defined somewhere
 import VehicleTable from "./components/VehicleTable";
-import VehicleImageProcessor from "./components/VehicleImageProcessor";
+import { exportVehicleData } from "./utils/VehicleJSONGenerator";
 
 const App: React.FC = () => {
   const [updatedVehicles, setUpdatedVehicles] = useState<Vehicle[]>([]);
   const [isShowingThirdPartyImages, setIsShowingThirdPartyImages] = useState<boolean>(false);
-
-  const [imagesToProcess, setImagesToProcess] = useState<[Vehicle, string][] | null>(null);
 
   // Automatically trigger image fetching in VehicleImageFetcher after CSV is loaded
   const handleCsvLoaded = (vehicles: Vehicle[]) => {
@@ -21,8 +19,8 @@ const App: React.FC = () => {
   }
 
   const processNewVehicleImages = (vehicleImages: [Vehicle, string][]) => {
-    setImagesToProcess(vehicleImages);
     console.log(vehicleImages);
+    exportVehicleData(vehicleImages)
   }
 
   return (
@@ -32,25 +30,18 @@ const App: React.FC = () => {
       {/* CSV Upload or URL Parsing */}
 
       {/* Show the list of vehicles in a table and fetch images */}
-      {imagesToProcess === null ? (
-        <div>
-          <CsvUploader setUpdatedVehicles={handleCsvLoaded} />
+      <div>
+        <CsvUploader setUpdatedVehicles={handleCsvLoaded} />
 
-          <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-            <button onClick={showThirdPartyImages} disabled={updatedVehicles.length == 0}>Load 3rd-party images</button>
-          </div>
-          <VehicleTable 
-            vehicles={updatedVehicles/*.slice(0, 3) Limit to first 3 vehicles for debuggings*/} 
-            shouldShowThirdPartyImages={isShowingThirdPartyImages}
-            continueWithVehicleURLS={processNewVehicleImages}
-          />
+        <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+          <button onClick={showThirdPartyImages} disabled={updatedVehicles.length == 0}>Load 3rd-party images</button>
         </div>
-      ) : (
-        <div>
-          <button onClick={() => setImagesToProcess(null)}>Cancel</button>
-          <VehicleImageProcessor vehicleURLs={imagesToProcess} />
-        </div>
-      )}
+        <VehicleTable 
+          vehicles={updatedVehicles/*.slice(0, 3) Limit to first 3 vehicles for debuggings*/} 
+          shouldShowThirdPartyImages={isShowingThirdPartyImages}
+          continueWithVehicleURLS={processNewVehicleImages}
+        />
+      </div>
     </div>
   );
 };
