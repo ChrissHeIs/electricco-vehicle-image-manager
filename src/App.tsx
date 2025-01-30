da55b1e34528f32b67b73759f30ea8ae37bebb71
Pavlo Shestak <shestak.pavel@gmail.com>
2025-01-30 10:48:17 +0200

    Implementing merging of existing JSON list and overridden values

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,39 @@ import VehicleTable from "./components/VehicleTable";
 import { exportVehicleData } from "./utils/VehicleJSONGenerator";
 import ImagesFileUploader from "./components/ImagesFileUploader";
 
+type VehicleImagePair = [Vehicle, string];
+
+function areVehiclesEqual(v1: Vehicle, v2: Vehicle): boolean {
+  return (
+    v1.brand === v2.brand &&
+    v1.model === v2.model &&
+    v1.year === v2.year
+  );
+}
+
+function mergeVehicleImages(
+  originalImages: VehicleImagePair[],
+  overriddenImages: VehicleImagePair[]
+): VehicleImagePair[] {
+  const result = [...originalImages];
+
+  overriddenImages.forEach(([overriddenVehicle, overriddenUrl]) => {
+    const existingIndex = result.findIndex(([vehicle]) => 
+      areVehiclesEqual(vehicle, overriddenVehicle)
+    );
+
+    if (existingIndex >= 0) {
+      // Replace existing entry
+      result[existingIndex] = [overriddenVehicle, overriddenUrl];
+    } else {
+      // Append new entry
+      result.push([overriddenVehicle, overriddenUrl]);
+    }
+  });
+
+  return result;
+}
+
 const App: React.FC = () => {
   const [vehicleImagesFromServer, setVehicleImagesFromServer] = useState<[Vehicle, string][]>([])
   const [updatedVehicles, setUpdatedVehicles] = useState<Vehicle[]>([]);
@@ -22,7 +55,8 @@ const App: React.FC = () => {
 
   const processNewVehicleImages = (vehicleImages: [Vehicle, string][]) => {
     console.log(vehicleImages);
-    exportVehicleData(vehicleImages)
+    const mergedList = mergeVehicleImages(vehicleImagesFromServer, vehicleImages)
+    exportVehicleData(mergedList)
   }
 
   const handleVehicleImagesFromJSONFile = (vehicleImages: [Vehicle, string][]) => {
